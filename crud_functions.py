8deb5acec0268a5736aec2372fd3eeea79332d6e
NixonRus <167446421+NixonRus@users.noreply.github.com>
2024-10-05 21:18:36 +0700

    Add files via upload

---
+++ crud_functions.py
@@ -0,0 +1,42 @@
+```import sqlite3
+
+
+def initiate_db():
+    connection = sqlite3.connect('Product.db')
+    cursor = connection.cursor()
+
+    cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Products(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT,
+    price INTEGER NOT NULL
+    )
+    ''')
+    Products = [
+        ('Вишня', 'Гейнер со вкусом вишни', 100),
+        ('Клубника', 'Гейнер со вкусом клубники', 200),
+        ('Персик-Маракуйя', 'Гейнер со вкусом персик-маракуйя', 300),
+        ('Шоколад', 'Гейнер со вкусом шоколада', 400)
+    ]
+    # cursor.executemany('INSERT INTO Products (title, description, price) VALUES (?, ?, ?)', Products)
+    # cursor.execute('DELETE FROM Products')
+
+    connection.commit()
+    connection.close()
+
+
+def get_all_products():
+    connection = sqlite3.connect('Product.db')
+    cursor = connection.cursor()
+    cursor.execute('SELECT title, description, price FROM Products')
+    prods = cursor.fetchall()
+    for prod in prods:
+        return prod
+
+    connection.commit()
+    connection.close()
+
+
+# initiate_db()
+# get_all_products()```
